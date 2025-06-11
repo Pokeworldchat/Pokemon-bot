3f9465a8e6541c373eb859f1a45091c68d28a216
Pokeworldchat <pokemonworldchat@gmail.com>
2025-06-11 04:59:06 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+
+import telebot
+import random
+
+TOKEN = 'AQUI_TU_TOKEN'
+bot = telebot.TeleBot(TOKEN)
+
+pokemones = ['Pikachu', 'Charmander', 'Squirtle', 'Bulbasaur', 'Eevee']
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "¡Bienvenido a Pokémon World! Usa /pokemon para encontrar uno salvaje.")
+
+@bot.message_handler(commands=['pokemon'])
+def lanzar_pokemon(message):
+    elegido = random.choice(pokemones)
+    bot.send_message(message.chat.id, f"¡Un {elegido} apareció salvaje! ¿Lo vas a atrapar?")
+
+bot.infinity_polling()
